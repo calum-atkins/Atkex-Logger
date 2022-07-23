88a8d28cd6fab970bac893b67ce52ed49c1e2818
calum-atkins <calum2atkins@gmail.com>
2022-07-23 20:52:49 +0100

    Units, side, start price, sl price, tp price and formula dded to new record on add click

--- src/main/java/MainController.java
+++ src/main/java/MainController.java
@@ -5,7 +5,10 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -16,6 +19,8 @@ import java.util.*;
 public class MainController implements Initializable {
     @FXML
     ComboBox indexCombobox;
+    @FXML
+    ComboBox sideCombobox;
 
     @FXML
     Label lblLabel;
@@ -25,23 +30,30 @@ public class MainController implements Initializable {
 
     @FXML
     TextField txtUnits;
+    @FXML
+    TextField txtStartPrice;
+    @FXML
+    TextField txtStopLossPrice;
+    @FXML
+    TextField txtTakeProfitPrice;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         indexCombobox.getItems().clear();
+
+        sideCombobox.getItems().addAll("Long","Short");
 
     }
 
     @FXML void addClick() {
         System.out.println(indexCombobox.getSelectionModel().getSelectedItem().toString() + " "+ txtUnits.getText());
 
-
-        File myFile = new File("src/main/resources/spreadsheets/AUDCAD.xlsx");
+        System.out.println("src/main/resources/spreadsheets/" + indexCombobox.getSelectionModel().getSelectedItem().toString());
+        File myFile = new File("src/main/resources/spreadsheets/" + indexCombobox.getSelectionModel().getSelectedItem().toString());
 
         try {
 
             FileInputStream fis = new FileInputStream(myFile);
-
 
             // Finds the workbook instance for XLSX file
             XSSFWorkbook myWorkBook = new XSSFWorkbook (fis);
@@ -49,13 +61,36 @@ public class MainController implements Initializable {
             //Get sheet
             XSSFSheet mySheet = myWorkBook.getSheetAt(0);
 
+            //Assign values to object
             Map<String, Object[]> data = new HashMap<String, Object[]>();
-            data.put("34", new Object[] {34d, "Test", "Test2"});
+            data.put("1", new Object[] {
+                    txtUnits.getText().toString(),
+                    sideCombobox.getSelectionModel().getSelectedItem().toString(),
+                    txtStartPrice.getText().toString(),
+                    txtStopLossPrice.getText().toString(),
+                    txtTakeProfitPrice.getText().toString(),
+                    "=C10+A2",
+                    0,
+                    "SL Profit",
+                    "TP Profit",
+                    "NA",
+                    "Date",
+                    "Settled",
+                    "Duration",
+                    "Pips pl",
+                    "Profit",
+                    "Desc"
+            });
 
             // Set to Iterate and add rows into XLS file
             Set<String> newRows = data.keySet();
+
             // get the last row number to append new data
-            int rownum = mySheet.getLastRowNum();
+            int rownum = mySheet.getLastRowNum() + 1;
+            System.out.println(rownum);
+
+            FormulaEvaluator evaluator = myWorkBook.getCreationHelper().createFormulaEvaluator();
+
             for (String key : newRows) {
                 // Creating a new Row in existing XLSX sheet
                 Row row = mySheet.createRow(rownum++);
@@ -63,6 +98,16 @@ public class MainController implements Initializable {
                 int cellnum = 0;
                 for (Object obj : objArr) {
                     Cell cell = row.createCell(cellnum++);
+                    if(obj.toString().startsWith("=")) {
+                        String formula = obj.toString().substring(1);
+                        System.out.println(formula);
+                        cell.setCellFormula(formula);
+
+//                        XSSFFormulaEvaluator formulaEvaluator =
+//                                myWorkBook.getCreationHelper().createFormulaEvaluator();
+//                        formulaEvaluator.evaluateFormulaCell(cell);
+                    }
+
                     if (obj instanceof String) {
                         cell.setCellValue((String) obj);
                     } else if (obj instanceof Boolean) {
@@ -75,8 +120,13 @@ public class MainController implements Initializable {
                 }
             }
             FileOutputStream os = new FileOutputStream(myFile);
+            evaluator.evaluateAll();
             myWorkBook.write(os);
             System.out.println("Writing on XLSX file Finished ...");
+
+            os.close();
+            myWorkBook.close();
+            fis.close();
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -91,5 +141,6 @@ public class MainController implements Initializable {
         for (int i = 0; i < markets.size(); i++) {
             indexCombobox.getItems().add(markets.get(i));
         }
+
     }
 }
